41a16b925e8cacb943a2bca26231146a46dbf763
adamskipadamski <adam.t.wright@outlook.com>
2021-11-03 23:13:46 +0000

    add Buggedometer component and place in Level1

---
+++ Assets/Resources/Scripts/Buggedometer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Buggedometer : MonoBehaviour
+{
+    public int Patience;
+    public int OutOfPatienceDelaySeconds;
+    public UnityEvent OnOutOfPatience;
+
+    public void DecrementPatience()
+    {
+        Patience -= 1;
+
+        if (Patience == 0)
+        {
+            StartCoroutine(OutOfPatience());
+        }
+    }
+
+    private IEnumerator OutOfPatience()
+    {
+        yield return new WaitForSeconds(OutOfPatienceDelaySeconds);
+
+        OnOutOfPatience.Invoke();
+    }
+}
