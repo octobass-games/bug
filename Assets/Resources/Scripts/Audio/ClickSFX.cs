4117984073dfd66cb6b926d45bbc857c03c1498b
Halden <halden@theparkes.me.uk>
2021-11-04 10:55:08 +0000

    Update ClickSFX.cs

--- Assets/Resources/Scripts/Audio/ClickSFX.cs
+++ Assets/Resources/Scripts/Audio/ClickSFX.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class ClickSFX : MonoBehaviour
 {
+    bool isWrong = true;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && isWrong == true)
         {
             FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/clickWrong");
         }
+
+        else if (Input.GetMouseButtonDown(0) && isWrong == false)
+        {
+            //FMODUnity.RuntimeManager.PlayOneShot("event:/");
+        }
+    }
     }
 }
