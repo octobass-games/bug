c3c2ea110626711ea3bd0a23af2982855a69bad3
adamskipadamski <adam.t.wright@outlook.com>
2021-11-05 10:49:38 +0000

    add ghost when dragging

--- Assets/Resources/Scripts/Input/Draggable.cs
+++ Assets/Resources/Scripts/Input/Draggable.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Collider2D))]
 public class Draggable : MonoBehaviour
 {
+    private GameObject Ghost;
     private Vector3 PositionBeforeDragging;
     private bool IsDragging;
     private Collider2D[] OverlappingColliders = new Collider2D[1];
@@ -11,6 +12,18 @@ public class Draggable : MonoBehaviour
     {
         useTriggers = true
     };
+
+    void Start()
+    {
+        var spriteRendererSprite = GetComponent<SpriteRenderer>().sprite;
+
+        Ghost = new GameObject("ghost");
+        var ghostSpriteRenderer = Ghost.AddComponent<SpriteRenderer>();
+        ghostSpriteRenderer.sprite = spriteRendererSprite;
+        ghostSpriteRenderer.color = new Color(ghostSpriteRenderer.color.r, ghostSpriteRenderer.color.g, ghostSpriteRenderer.color.b, 0.4f);
+        Ghost.transform.SetParent(this.transform.parent);
+        Ghost.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+    }
 
     void Update()
     {
@@ -34,6 +47,7 @@ public class Draggable : MonoBehaviour
         if (GetComponent<BoxCollider2D>().OverlapCollider(ContactFilter, OverlappingColliders) > 0)
         {
             OverlappingColliders[0].gameObject.GetComponent<Combinable>()?.Combine(gameObject);
+            Destroy(Ghost);
         }
         else
         {
