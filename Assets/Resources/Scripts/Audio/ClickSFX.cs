cdc053fe024c32564dca57bcb8284f912ce9fbc8
Halden <halden@theparkes.me.uk>
2021-11-09 13:37:41 +0000

    loopable sfx added

--- Assets/Resources/Scripts/Audio/ClickSFX.cs
+++ Assets/Resources/Scripts/Audio/ClickSFX.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class ClickSFX : MonoBehaviour
 {
-    public void PlayOneShot(string path)
+    FMOD.Studio.EventInstance loopSound;
+
+    public string path;
+
+    bool isPlaying = false;
+
+    private void Start()
+    {
+        loopSound = FMODUnity.RuntimeManager.CreateInstance(path);
+    }
+
+    public void PlayOneShot()
     {
         FMODUnity.RuntimeManager.PlayOneShot(path);
+    }
+
+    public void PlayLoop(string Path)
+    {
+        if (isPlaying == false)
+        {
+            loopSound.start();
+            isPlaying = true;
+        }
+
+        else if (isPlaying == true)
+        {
+            loopSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            isPlaying = false;
+        }
     }
 }
