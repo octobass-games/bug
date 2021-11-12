f56b91aa08f0334c5de5057c5a69d4aa7abc50a2
Megan <megan@theparkes.me.uk>
2021-11-12 10:26:03 +0000

    bath tap animations

---
+++ Assets/Resources/Scripts/AnimationUtil.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AnimationUtil : MonoBehaviour
+{
+    public Animator Animator;
+    public void SetBoolTrue(string name)
+    {
+        Animator.SetBool(name, true);
+    }
+
+    public void SetBoolFalse(string name)
+    {
+        Animator.SetBool(name, false);
+    }
+
+
+    public void ToggleBool(string name)
+    {
+        Animator.SetBool(name, !Animator.GetBool(name));
+    }
+
+}
---
+++ Assets/Resources/Scripts/StateSwitcher.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StateSwitcher : MonoBehaviour
+{
+    private bool ThingOneOn = false;
+    private bool ThingTwoOn = false;
+
+    public UnityEvent OnThingOneOn;
+    public UnityEvent OnThingTwoOn;
+    public UnityEvent OnBothOn;
+    public UnityEvent OnBothOff;
+
+    public void ToggleThingOne()
+    {
+        ThingOneOn = !ThingOneOn;
+        Logic();
+    }
+
+    public void ToggleThingTwo()
+    {
+        ThingTwoOn = !ThingTwoOn;
+        Logic();
+    }
+
+    private void Logic()
+    {
+        if (ThingOneOn && ThingTwoOn)
+        {
+            OnBothOn.Invoke();
+            return;
+        }
+
+        if (ThingOneOn && !ThingTwoOn)
+        {
+            OnThingOneOn.Invoke();
+            return;
+        }
+
+        if (!ThingOneOn && ThingTwoOn)
+        {
+            OnThingTwoOn.Invoke();
+            return;
+        }
+
+        OnBothOff.Invoke();
+    }
+
+}
