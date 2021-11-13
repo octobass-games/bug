8725c885fe3da49ceac45bd30442b4c4a0fb4045
Megan <megan@theparkes.me.uk>
2021-11-13 18:04:26 +0000

    make meter slide

--- Assets/Resources/Scripts/BuggedometerRenderer.cs
+++ Assets/Resources/Scripts/BuggedometerRenderer.cs
@@ -3,25 +3,44 @@
 public class BuggedometerRenderer : MonoBehaviour
 {
     public SpriteRenderer Bar;
+    private Vector3 Aim;
+    private bool inProgress;
+    private float Speed = 3;
+    private float z;
 
     void Awake()
     {
-        Bar.transform.localScale = new Vector2(1, 0);
+        z = Bar.transform.lossyScale.z;
+        Bar.transform.localScale = new Vector3(1, 0, z);
     }
 
     public void Render(float currentPatience, float maxPatience)
     {
         if (currentPatience == maxPatience)
         {
-            Bar.transform.localScale = new Vector2(1, 0);
+            Aim = new Vector3(1, 0, z);
         }
         else if (currentPatience == 0)
         {
-            Bar.transform.localScale = new Vector2(1, 1);
+            Aim = new Vector3(1, 1, z);
         }
         else
         {  
-            Bar.transform.localScale = new Vector2(1, 1- (currentPatience / maxPatience));
+            Aim = new Vector3(1, 1- (currentPatience / maxPatience), z);
+        }
+        inProgress = true;
+    }
+
+    void Update()
+    {
+        if (!inProgress) return;
+        
+        float step = Speed * Time.deltaTime;
+        Bar.transform.localScale = Vector3.MoveTowards(Bar.transform.localScale, Aim, step);
+
+        if (Bar.transform.localScale == Aim)
+        {
+            inProgress = false;
         }
     }
 }
