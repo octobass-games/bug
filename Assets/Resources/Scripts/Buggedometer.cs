4f3d0bc2ed1516df4d2d265e8449f8f9d1cd34e5
Megan <megan@theparkes.me.uk>
2021-11-13 14:01:58 +0000

    add bugged-o-meter to some scenes

--- Assets/Resources/Scripts/Buggedometer.cs
+++ Assets/Resources/Scripts/Buggedometer.cs
@@ -6,10 +6,18 @@ using UnityEngine.Events;
 public class Buggedometer : MonoBehaviour
 {
     public int Patience;
+    private int MaxPatience;
     public int OutOfPatienceDelaySeconds;
     public UnityEvent OnOutOfPatience;
+    public BuggedometerRenderer BuggedometerRenderer;
 
     private HashSet<GameObject> PatienceDroppers = new HashSet<GameObject>();
+    private HashSet<GameObject> PatienceAdders = new HashSet<GameObject>();
+
+    void Awake()
+    {
+        MaxPatience = Patience;
+    }
 
     public void DecrementPatience(GameObject gameObject)
     {
@@ -18,12 +26,33 @@ public class Buggedometer : MonoBehaviour
             Patience -= 1;
             PatienceDroppers.Add(gameObject);
 
+            BuggedometerRenderer.Render(Patience, MaxPatience);
+
+            if (PatienceAdders.Contains(gameObject))
+            {
+                PatienceAdders.Remove(gameObject);
+            }
+
             if (Patience == 0)
             {
                 StartCoroutine(OutOfPatience());
             }
         }
     }
+
+
+    public void IncrementPatience(GameObject gameObject)
+    {
+        if (PatienceDroppers.Contains(gameObject) && !PatienceAdders.Contains(gameObject) && Patience != MaxPatience)
+        {
+            Patience += 1;
+            PatienceAdders.Add(gameObject);
+            PatienceDroppers.Remove(gameObject);
+
+            BuggedometerRenderer.Render(Patience, MaxPatience);
+        }
+    }
+
 
     private IEnumerator OutOfPatience()
     {
---
+++ Assets/Resources/Scripts/BuggedometerRenderer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BuggedometerRenderer : MonoBehaviour
+{
+    public SpriteRenderer Bar;
+
+    void Awake()
+    {
+        Bar.transform.localScale = new Vector2(1, 0);
+    }
+
+    public void Render(float currentPatience, float maxPatience)
+    {
+        if (currentPatience == maxPatience)
+        {
+            Bar.transform.localScale = new Vector2(1, 0);
+        }
+        else if (currentPatience == 0)
+        {
+            Bar.transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {  
+            Bar.transform.localScale = new Vector2(1, 1- (currentPatience / maxPatience));
+        }
+    }
+}
