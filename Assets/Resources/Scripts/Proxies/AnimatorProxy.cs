cb403a03496a90eb4fb9b3c9dc3e1f3aca3985a4
adamskipadamski <adam.t.wright@outlook.com>
2021-11-14 13:46:41 +0000

    rename AnimationUtil to AnimatorProxy and place in Proxies directory

---
+++ Assets/Resources/Scripts/Proxies/AnimatorProxy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AnimatorProxy : MonoBehaviour
+{
+    public Animator Animator;
+
+    public void SetBoolTrue(string name) => Animator.SetBool(name, true);
+
+    public void SetBoolFalse(string name) => Animator.SetBool(name, false);
+
+    public void ToggleBool(string name) => Animator.SetBool(name, !Animator.GetBool(name));
+
+    public void SetLayerWeightToOne(int index) => Animator.SetLayerWeight(index, 1);
+
+    public void SetLayerWeightToZero(int index) => Animator.SetLayerWeight(index, 0);
+}
