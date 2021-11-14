f22efd9d3b396f3dbddfbf9ba9b57216cf6215ba
Megan <megan@theparkes.me.uk>
2021-11-14 15:49:20 +0000

    add pizza

--- Assets/Resources/Scripts/EventInvoker.cs
+++ Assets/Resources/Scripts/EventInvoker.cs
@@ -5,7 +5,9 @@ public class EventInvoker: MonoBehaviour
 {
     public UnityEvent Event;
     public UnityEvent Event2;
+    public UnityEvent Event3;
 
     public void InvokeCustomEvent() => Event.Invoke();
     public void InvokeCustomEvent2() => Event2.Invoke();
+    public void InvokeCustomEvent3() => Event3.Invoke();
 }
