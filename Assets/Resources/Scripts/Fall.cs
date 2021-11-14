bd6a7db8fe8a0bdd36db26c452377b15fcfc3512
adamskipadamski <adam.t.wright@outlook.com>
2021-11-14 10:27:53 +0000

    improve Fall logic

--- Assets/Resources/Scripts/Fall.cs
+++ Assets/Resources/Scripts/Fall.cs
@@ -1,25 +1,58 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Fall : MonoBehaviour
 {
-    public Transform Destination;
-    public float Speed;
-    public float Delay;
+    public Transform InitialPosition;
+    public Transform RestingPosition;
+    public Transform FinalPosition;
+    public float Speed = 1;
+    public float PreFallDelaySeconds;
+    public bool IsFallingToRestingPosition = false;
+    public bool InitialRoom;
 
     private bool IsPushing;
+
+    private void Awake()
+    {
+        transform.position = InitialPosition.position;
+    }
+
+    private void Start()
+    {
+        if (InitialRoom)
+        {
+            Push();
+        }
+    }
 
     void Update()
     {
         if (IsPushing)
         {
+            var destination = IsFallingToRestingPosition ? RestingPosition : FinalPosition;
+
             float step = Speed * Time.deltaTime;
 
-            transform.position = Vector3.MoveTowards(transform.position, Destination.position, step);
+            transform.position = Vector3.MoveTowards(transform.position, destination.position, step);
 
-            if (transform.position == Destination.position)
+            if (transform.position == destination.position)
             {
                 IsPushing = false;
+
+                bool isAtFinalPosition = !IsFallingToRestingPosition;
+
+                if (isAtFinalPosition)
+                {
+                    transform.position = InitialPosition.position;
+                    IsFallingToRestingPosition = true;
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    IsFallingToRestingPosition = false;
+                }
             }
         }
     }
@@ -31,7 +64,7 @@ public class Fall : MonoBehaviour
 
     private IEnumerator StartPushing()
     {
-        yield return new WaitForSeconds(Delay);
+        yield return new WaitForSeconds(PreFallDelaySeconds);
 
         IsPushing = true;
     }
