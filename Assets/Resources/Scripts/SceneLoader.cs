4a101813fd47d217f8742ceaa15c4c6ea56d51bf
Megan <megan@theparkes.me.uk>
2021-11-14 11:01:04 +0000

    update level 4 sprite

--- Assets/Resources/Scripts/SceneLoader.cs
+++ Assets/Resources/Scripts/SceneLoader.cs
@@ -5,6 +5,8 @@ public static class SceneLoader
 {
     public static void LoadScene(LevelData level, LevelData OldLevel)
     {
+        Debug.Log("load level: " + level);
+        Debug.Log("Unload level: " + OldLevel);
         if (OldLevel)
         {
             SceneManager.UnloadSceneAsync(OldLevel.SceneName);
