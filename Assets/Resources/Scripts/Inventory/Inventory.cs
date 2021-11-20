b40208a9683cc10e2c55f9cac45913ba4f3af401
adamskipadamski <adam.t.wright@outlook.com>
2021-11-20 14:43:26 +0000

    fix draggable issue
    fix draggable issue where the draggable
    position was being set wrong in DragEnd
    when called while room was changing

--- Assets/Resources/Scripts/Inventory/Inventory.cs
+++ Assets/Resources/Scripts/Inventory/Inventory.cs
@@ -1,31 +1,31 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Inventory : MonoBehaviour
 {
     public List<GameObject> Items;
     public GameObject InventoryItemBackground;
+    [FormerlySerializedAs("SpawnPoint")]
     public Transform SpawnPoint;
 
     private const int SpaceBetweenItems = -40;
 
     public void Add(GameObject item)
     {
-        item.AddComponent<Draggable>();
-
         Items.Add(item);
 
-        item.transform.SetParent(transform);
-        item.GetComponent<SpriteRenderer>().sortingLayerName = "Inventory";
-        item.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
-
         var yOffset = SpaceBetweenItems * (Items.Count - 1);
-        item.transform.position = new Vector3(transform.position.x, transform.position.y + yOffset, item.transform.position.z);
 
         var inventoryItemBackground = Instantiate(InventoryItemBackground);
         inventoryItemBackground.transform.position = new Vector3(SpawnPoint.position.x, SpawnPoint.position.y + yOffset, InventoryItemBackground.transform.position.z);
         inventoryItemBackground.transform.SetParent(transform.parent);
         var inventoryItemBackgroundCenter = inventoryItemBackground.GetComponent<SpriteRenderer>().bounds.center;
+
+        item.AddComponent<Draggable>();
+        item.GetComponent<SpriteRenderer>().sortingLayerName = "Inventory";
+        item.transform.SetParent(transform);
+        item.transform.localScale *= 0.75f;
         item.transform.position = new Vector3(inventoryItemBackgroundCenter.x, inventoryItemBackgroundCenter.y, item.transform.position.z);
     }
 }
--- Assets/Resources/Scripts/RoomSwitcher.cs
+++ Assets/Resources/Scripts/RoomSwitcher.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-
 public class RoomSwitcher : MonoBehaviour
 {
     public List<GameObject> Rooms;
