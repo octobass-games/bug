2e15f86eaa460c5cc2e292426572c65d67b9e9d6
Megan <megan@theparkes.me.uk>
2021-11-21 23:29:36 +0000

    update vampire scene

---
+++ Assets/Resources/Scripts/Proxies/CursorProxy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CursorProxy : MonoBehaviour
+{
+    private CustomCursor CustomCursor;
+    // Use this for initialization
+    void Awake()
+    {
+        CustomCursor = FindObjectOfType<CustomCursor>();
+    }
+
+    public void ResetCursor()
+    {
+        CustomCursor.SetNeutralCursor();
+    }
+}
