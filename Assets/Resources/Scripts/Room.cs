89d8118ec57bd08e4a55320e8be31ba1d69a2f9d
adamskipadamski <adam.t.wright@outlook.com>
2021-11-30 17:44:43 +0000

    update room switching in Level10

---
+++ Assets/Resources/Scripts/Room.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject Camera;
+    public bool IsInitialRoom;
+
+    private Coroutine FallingOutCoroutine;
+
+    void Awake()
+    {
+        if (!IsInitialRoom)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void FallIn()
+    {
+        if (FallingOutCoroutine != null)
+        {
+            StopCoroutine(FallingOutCoroutine);
+        }
+
+        var fallIns = GetComponentsInChildren<Fall>().ToList();
+
+        fallIns.ForEach(fallIn => fallIn.FallIn());
+    }
+
+    public void FallOut()
+    {
+        var fallIns = GetComponentsInChildren<Fall>().ToList();
+
+        FallingOutCoroutine = StartCoroutine(WaitThen(() => fallIns.ForEach(fallIn => fallIn.FallOut())));
+    }
+
+    IEnumerator WaitThen(Action cb)
+    {
+        yield return new WaitForSeconds(1);
+        cb();
+    }
+}
